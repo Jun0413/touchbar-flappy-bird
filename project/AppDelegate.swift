f527e273b3461c0a3af7469703e143ea0d952b60
jun0413 <jzeng004@e.ntu.edu.sg>
2019-01-18 20:53:56 +0800

    Initial Commit

---
+++ project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  project
+//
+//  Created by Junhao Zeng on 2019/1/18.
+//  Copyright © 2019 Junhao Zeng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Junhao Zeng on 2019/1/18.
+//  Copyright © 2019 Junhao Zeng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
